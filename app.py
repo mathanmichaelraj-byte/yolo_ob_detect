7e16b379a6ddd5522482a39336a72bcf444a8e80
Mathan <mathanmichaelraja.2004@gmail.com>
2026-01-21 21:04:46 +0530

    Minor Updates

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
 from ultralytics import YOLO
 import cv2
 
-model = YOLO("yolov10s.pt")
+model = YOLO('yolov10s.pt')
 cap = cv2.VideoCapture(0)
 
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-
-
 while cap.isOpened():
-    ret, frame = cap.read()
+    ret ,frame = cap.read()
+
     if not ret:
         break
 
-    results = model(frame, imgsz=960, conf=0.5, iou = 0.5)
+    results = model(frame)
+
     cv2.imshow("YOLO", results[0].plot())
 
-    if cv2.waitKey(1) & 0xFF in (27, ord('q')):
+    if cv2.waitKey(27) & 0xFF == ord('q'):
         break
 
     if cv2.getWindowProperty("YOLO", cv2.WND_PROP_VISIBLE) < 1:
         break
 
-cap.release()
+cap.release() 
 cv2.destroyAllWindows()
